e2cd9ae1c8fd04cffd925ab0308e6bb4432e79ea
crc-32 <me@crc32.dev>
2020-04-24 19:17:27 +0100

    Error applet on switch only

--- error.cpp
+++ error.cpp
@@ -3,14 +3,18 @@
 #include <sstream>
 
 void ErrorMsg(Result res, std::string shortMsg, std::string bigDesc) {
+#ifdef SWITCH
     ErrorApplicationConfig con;
     errorApplicationCreate(&con, shortMsg.c_str(), (bigDesc == "" ? NULL : bigDesc.c_str()));
     errorApplicationSetNumber(&con, res);
     errorApplicationShow(&con);
+#endif
 }
 
 void _Assert(std::string conditionTxt, std::string file, int line) {
+#ifdef SWITCH
     std::stringstream msg;
     msg << "Assert fail: " << conditionTxt << " at:" << std::endl << file << ":" << line;
     ErrorMsg(0, msg.str());
+#endif
 }
